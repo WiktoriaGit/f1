1278f9701803b4f8dafab3dba97e8214a720d46a
Fałowska_Wiktoria <wiktoriagithub@gmail.com>
2025-04-30 21:54:58 +0200

    nowe

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === KONFIGURACJA ===
-const BACKEND_URL ="https://0b7d-37-47-71-1.ngrok-free.app"; // lokalny backend
+const BACKEND_URL ="https://098a-37-47-71-1.ngrok-free.app"; // lokalny backend
 
 // Obsługa logowania użytkownika
 document.getElementById("loginForm")?.addEventListener("submit", async function (event) {
